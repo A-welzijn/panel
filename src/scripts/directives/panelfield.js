146e07f67e0c00d4ca61a4c9a456fbae76665986
Mathias Schaemelhout <mathias.schaemelhout@digipolis.be>
2015-06-11 14:41:52 +0200

    no message

---
+++ src/scripts/directives/panelfield.js
@@ -0,0 +1,36 @@
+'use strict';
+(function (module) {
+	try {
+		module = angular.module('awelzijn.panel');
+	} catch (e) {
+		module = angular.module('awelzijn.panel', []);
+	}
+	module.directive('aWelzijnPanelField', [function () {
+		return {
+			restrict: 'E',
+			replace: true,
+			transclude: true,
+			scope: {
+				title: '@',
+				label: '@',
+				email: '@',
+				colspan: '=',
+				editMode: '=',
+				filter: '@',
+				ngClick: '&'
+			},
+			templateUrl: 'templates/panelfield.html',
+			link: function (scope, element, attrs) {
+				if (scope.colspan) {
+					scope.colspanClass = ("col-lg-@ col-md-@").replace(/@/g, scope.colspan);
+				} else {
+					scope.colspanClass = ("col-lg-@ col-md-@").replace(/@/g, '6');
+				}
+
+				scope.hasClickCallback = function () {
+					return angular.isDefined(attrs.ngClick);
+				}
+			}
+		};
+	}]);
+})();
